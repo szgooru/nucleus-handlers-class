5f12724fcc657176a141511ae1a7ea6fda5f052d
Ashish Mishra <ashish@gooru.org>
2015-12-25 12:22:55 +0530

    Initial repo creation

---
+++ src/main/java/org/gooru/nucleus/handlers/classes/bootstrap/ClassVerticle.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.classes.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class ClassVerticle extends AbstractVerticle {
+}
