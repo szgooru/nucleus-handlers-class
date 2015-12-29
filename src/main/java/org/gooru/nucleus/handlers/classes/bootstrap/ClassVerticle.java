466b6772616378fad02316895f19ffe0ae911c72
Ashish Mishra <ashish@gooru.org>
2015-12-29 20:07:10 +0530

    Provide correct log file name, complete the future in main verticle, update build readme

--- src/main/java/org/gooru/nucleus/handlers/classes/bootstrap/ClassVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/classes/bootstrap/ClassVerticle.java
@@ -27,6 +27,7 @@ public class ClassVerticle extends AbstractVerticle {
 
     vertx.executeBlocking(blockingFuture -> {
       startApplication();
+      blockingFuture.complete();
     }, future -> {
       if (future.succeeded()) {
         voidFuture.complete();
