d000bd6280e0946a85d24ca188a9b4f78785cf30
am-gooru <ashish@gooru.org>
2016-02-12 05:37:06 +0530

    Provide implementation of authorizer to check either owner or collaborator

--- src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/dbauth/ClassOwnerOrCollaboratorAuthorizer.java
+++ src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/dbauth/ClassOwnerOrCollaboratorAuthorizer.java
@@ -1,19 +1,59 @@
 package org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.dbauth;
 
+import io.vertx.core.json.JsonArray;
 import org.gooru.nucleus.handlers.classes.processors.ProcessorContext;
 import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entities.AJEntityClass;
 import org.gooru.nucleus.handlers.classes.processors.responses.ExecutionResult;
 import org.gooru.nucleus.handlers.classes.processors.responses.MessageResponse;
+import org.gooru.nucleus.handlers.classes.processors.responses.MessageResponseFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ResourceBundle;
 
 /**
  * Created by ashish on 8/2/16.
  */
 class ClassOwnerOrCollaboratorAuthorizer implements Authorizer<AJEntityClass> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ClassOwnerAuthorizer.class);
+  private final ProcessorContext context;
+  private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("messages");
+
   public ClassOwnerOrCollaboratorAuthorizer(ProcessorContext context) {
+    this.context = context;
   }
 
   @Override
   public ExecutionResult<MessageResponse> authorize(AJEntityClass model) {
-    return null;
+    String courseId = model.getString(AJEntityClass.COURSE_ID);
+    if (courseId != null && !courseId.isEmpty()) {
+      LOGGER.warn("Delete request for class '{}' which is having a course '{}'", context.classId(), context.courseId());
+    }
+    if (checkOwner(model) || checkCollaborator(model)) {
+      return new ExecutionResult<>(null, ExecutionResult.ExecutionStatus.CONTINUE_PROCESSING);
+    }
+    return new ExecutionResult<>(MessageResponseFactory.createForbiddenResponse(RESOURCE_BUNDLE.getString("not.allowed")),
+      ExecutionResult.ExecutionStatus.FAILED);
+  }
+
+  private boolean checkOwner(AJEntityClass model) {
+    String creatorId = model.getString(AJEntityClass.CREATOR_ID);
+    if (creatorId == null || creatorId.isEmpty() || !creatorId.equalsIgnoreCase(context.userId())) {
+      LOGGER.warn("User '{}' is not owner of class '{}'", context.userId(), context.classId());
+      return false;
+    }
+    return true;
+  }
+
+  private boolean checkCollaborator(AJEntityClass model) {
+    String collaboratorString = model.getString(AJEntityClass.COLLABORATOR);
+    if (collaboratorString != null && !collaboratorString.isEmpty()) {
+      JsonArray collaborators = new JsonArray(collaboratorString);
+      if (collaborators.contains(context.userId())) {
+        return true;
+      }
+    }
+    LOGGER.warn("User '{}' is not collaborator of class '{}'", context.userId(), context.classId());
+    return false;
   }
 }
