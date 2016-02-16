e3b965979a8ebb0d02cdf462c6ed658528bade4f
am-gooru <ashish@gooru.org>
2016-02-16 12:14:15 +0530

    Provide update class handler

--- src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/dbhandlers/UpdateClassHandler.java
+++ src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/dbhandlers/UpdateClassHandler.java
@@ -3,14 +3,20 @@ package org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.db
 import io.vertx.core.json.JsonObject;
 import org.gooru.nucleus.handlers.classes.constants.MessageConstants;
 import org.gooru.nucleus.handlers.classes.processors.ProcessorContext;
+import org.gooru.nucleus.handlers.classes.processors.events.EventBuilderFactory;
+import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.dbauth.AuthorizerBuilder;
 import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entities.AJEntityClass;
+import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entitybuilders.EntityBuilder;
 import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.validators.PayloadValidator;
 import org.gooru.nucleus.handlers.classes.processors.responses.ExecutionResult;
 import org.gooru.nucleus.handlers.classes.processors.responses.MessageResponse;
 import org.gooru.nucleus.handlers.classes.processors.responses.MessageResponseFactory;
+import org.javalite.activejdbc.LazyList;
+import org.javalite.activejdbc.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
@@ -20,6 +26,7 @@ class UpdateClassHandler implements DBHandler {
   private static final Logger LOGGER = LoggerFactory.getLogger(UpdateClassHandler.class);
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("messages");
   private final ProcessorContext context;
+  private AJEntityClass entityClass;
 
   UpdateClassHandler(ProcessorContext context) {
     this.context = context;
@@ -58,12 +65,43 @@ class UpdateClassHandler implements DBHandler {
 
   @Override
   public ExecutionResult<MessageResponse> validateRequest() {
-    return null;
+    LazyList<AJEntityClass> classes = AJEntityClass.where(AJEntityClass.FETCH_QUERY_FILTER, context.classId());
+    if (classes.isEmpty()) {
+      LOGGER.warn("Not able to find class '{}'", this.context.classId());
+      return new ExecutionResult<>(MessageResponseFactory.createNotFoundResponse(RESOURCE_BUNDLE.getString("not.found")),
+        ExecutionResult.ExecutionStatus.FAILED);
+    }
+    this.entityClass = classes.get(0);
+    // Class should be of current version and Class should not be archived
+    if (!this.entityClass.isCurrentVersion() || this.entityClass.isArchived()) {
+      LOGGER.warn("Class '{}' is either archived or not of current version", context.classId());
+      return new ExecutionResult<>(
+        MessageResponseFactory.createInvalidRequestResponse(RESOURCE_BUNDLE.getString("class.archived.or.incorrect.version")),
+        ExecutionResult.ExecutionStatus.FAILED);
+    }
+
+    return AuthorizerBuilder.buildUpdateClassAuthorizer(this.context).authorize(this.entityClass);
   }
 
   @Override
   public ExecutionResult<MessageResponse> executeRequest() {
-    return null;
+    this.entityClass.setModifierId(this.context.userId());
+    new DefaultAJEntityClassBuilder().build(this.entityClass, context.request(), AJEntityClass.getConverterRegistry());
+
+    boolean result = this.entityClass.save();
+    if (!result) {
+      LOGGER.error("Class with id '{}' failed to save", context.classId());
+      if (this.entityClass.hasErrors()) {
+        Map<String, String> map = this.entityClass.errors();
+        JsonObject errors = new JsonObject();
+        map.forEach(errors::put);
+        return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(errors), ExecutionResult.ExecutionStatus.FAILED);
+      }
+    }
+    return new ExecutionResult<>(MessageResponseFactory
+      .createNoContentResponse(RESOURCE_BUNDLE.getString("updated"), EventBuilderFactory.getUpdateClassEventBuilder(context.classId())),
+      ExecutionResult.ExecutionStatus.SUCCESSFUL);
+
   }
 
   @Override
@@ -72,5 +110,8 @@ class UpdateClassHandler implements DBHandler {
   }
 
   private static class DefaultPayloadValidator implements PayloadValidator {
+  }
+
+  private static class DefaultAJEntityClassBuilder implements EntityBuilder<AJEntityClass> {
   }
 }
