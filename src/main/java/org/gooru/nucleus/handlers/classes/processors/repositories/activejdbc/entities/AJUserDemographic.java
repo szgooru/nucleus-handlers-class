61a3005e0685fb211aa9b78f0e75af1d7fd38eff
am-gooru <ashish@gooru.org>
2016-03-04 11:47:18 +0530

    Need to send ID as part of response and fixing of typo

--- src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/entities/AJUserDemographic.java
+++ src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/entities/AJUserDemographic.java
@@ -12,9 +12,10 @@ import java.util.List;
 @Table("user_demographic")
 public class AJUserDemographic extends Model {
   public static final String GET_SUMMARY_QUERY = "select id, firstname, lastname, thumbnail_path from user_demographic where id = ANY(?::uuid[])";
+  public static final String ID = "id";
   public static final String FIRST_NAME = "firstname";
-  public static final String LAST_NAME = "lastame";
+  public static final String LAST_NAME = "lastname";
   public static final String THUMBNAIL_PATH = "thumbnail_path";
-  public static final List<String> GET_SUMMARY_QUERY_FIELD_LIST = Arrays.asList(FIRST_NAME, LAST_NAME, THUMBNAIL_PATH);
+  public static final List<String> GET_SUMMARY_QUERY_FIELD_LIST = Arrays.asList(ID, FIRST_NAME, LAST_NAME, THUMBNAIL_PATH);
 
 }
